9e8da7ebccf55024b54e03aaf9f634cd035ca771
shuishuidewo <2868776500@qq.com>
2022-10-20 18:10:23 +0800

    init

---
+++ yeb-server/src/main/java/com/awei/server/controller/SysMsgContentController.java
@@ -0,0 +1,18 @@
+package com.awei.server.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ */
+@RestController
+@RequestMapping("/sys-msg-content")
+public class SysMsgContentController {
+
+}
